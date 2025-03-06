bad529d146e4d4120ad038857d06acf32872fb78
Sampreeth D S <sampreethdsgowda@gmail.com>
2025-03-06 17:28:50 +0530

    Vesion 1.3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to My Sample Python App!</h1><p>This is a simple Flask application with build version version 1.2</p>"
+    return "<h1>Welcome to My Sample Python App!</h1><p>This is a simple Flask application with build version version 1.3</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
